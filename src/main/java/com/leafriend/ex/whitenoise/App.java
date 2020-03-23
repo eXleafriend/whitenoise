8d3a2fb334c37cc2730804b8665f08475ce41f83
leafriend <leafriend@gamil.com>
2020-03-24 02:05:43 +0900

    메인 애플리케이션 구현

--- src/main/java/com/leafriend/ex/whitenoise/App.java
+++ src/main/java/com/leafriend/ex/whitenoise/App.java
@@ -1,13 +1,35 @@
 package com.leafriend.ex.whitenoise;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class App {
+
+	public static void main(String[] args) {
+		final Timer timer = new Timer();
+		final TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("play() @ " + new Date().toString());
+				play();
+			}
+		};
+		timer.scheduleAtFixedRate(task, new Date(), 200);
+	}
+
+	public static double[] tone(double hz, double amplitude) {
+		int n = (int) StdAudio.SAMPLE_RATE;
+		double[] a = new double[n + 1];
+		for (int i = 0; i <= n; i++) {
+			a[i] = amplitude * Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
+		}
+		return a;
+	}
+
+	public static void play() {
+		double[] a = tone(80, 10);
+		StdAudio.play(a);
+	}
+
 }
